799921088be3cd73579e93548599a767833cf7ac
kelsieszost <owner@kelsieszost.design>
2022-09-13 17:52:29 -0400

    added init function and prompts

---
+++ README.md
@@ -0,0 +1,21 @@
+
+
+    ## Project Title
+    ReadMe Generator
+    
+    ## Description
+    Generates a ReadME
+
+    ## Installation
+    Install this project locally
+
+    ## Usage
+    This project is used for generating read me
+
+    ## License
+    mit
+
+    ## Contributors
+    undefined
+
+    
---
+++ index.js
@@ -0,0 +1,86 @@
+// Packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
+function generateReadMe(answers) {
+    return `
+
+    ## Project Title
+    ${answers.projectTitle}
+    
+    ## Description
+    ${answers.description}
+
+    ## Installation
+    ${answers.installation}
+
+    ## Usage
+    ${answers.usage}
+
+    ## License
+    ${answers.license}
+
+    ## Contributors
+    ${answers.contributors}
+
+    `
+}
+
+// Array of questions for user input
+    const questions = () => { 
+        return inquirer.prompt([
+        {
+            type: "input",
+            name: "projectTitle",
+            message: "Enter project title"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Describe your project",
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Describe the installation process"
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Describe the project usage"
+        },
+        {
+            type: "input",
+            name: "license",
+            message: "Which license applies to your project?"
+        }
+    ]);
+}
+
+const writeFile = data => {
+    fs.writeFile('README.md', data, err => {
+        // if there is an error 
+        if (err) {
+            console.log(err);
+            return;
+        // when the README has been created 
+        } else {
+            console.log("Your README has been successfully created!")
+        }
+    })
+}; 
+
+questions()
+// getting user answers 
+.then(answers => {
+    return generateReadMe(answers);
+})
+// using data to display on page 
+.then(data => {
+    return writeFile(data);
+})
+// catching errors 
+.catch(err => {
+    console.log(err)
+})
